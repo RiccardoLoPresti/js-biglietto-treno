894536599ff7a9772acf25304f90fea9d1b4110f
RiccardoLoPresti <riccardolopresti35@gmail.com>
2022-10-12 17:06:58 +0200

    [add] css + script

--- js/script.js
+++ js/script.js
@@ -1,57 +1,101 @@
-
-const prezzoKm = 0.21;
-const scontoMinorenni = 0.2;
-const scontoOver = 0.4;
-let calcoloValido = true;
+/* DATA LAYER */
+const costForKm = 0.21;
+const minorDiscount = 0.2;
+const seniorDiscount = 0.4;
+let ValidData = true;
 let output;
 
-const numeroKmUtente = parseInt(prompt('Quanti km vuoi percorrere?'));
-const prezzoBiglietto = prezzoKm * numeroKmUtente;
+/*BUSINESS LAYER*/
+const btn1 = document.getElementById('get');
+btn1.addEventListener('click', function(){
+  const nameSurname = document.getElementById('name').value
+  const userNumberKm = document.getElementById('km').value
+  const userAge = document.getElementById('eta').value
 
+  const ticketCost = costForKm * userNumberKm;
+  console.log(('costo biglietto'), ticketCost);
 
-if(numeroKmUtente < 1 || isNaN(numeroKmUtente)){
-  alert('Inserisci un numero superiore o uguale a 1');
-  
-  calcoloValido = false;
-}
+  if(nameSurname === '' || !isNaN(nameSurname)){
+    alert('Si prega di inserire un Nome e un Cognome!');
 
-if(calcoloValido){
-  const etaUtente = parseInt(prompt('Quanti anni hai?'));
-
-  if(etaUtente >= 18 && etaUtente < 65){
-    output = prezzoBiglietto.toFixed(2); 
-  
-    document.getElementById('prezzo').innerHTML =`
-      Il prezzo del biglietto non è soggetto a sconti, il costo totale è di: <br> 
-      <span class="bg-info fw-bold">${output} €</span>
-    `; 
+    ValidData = false;
   }
 
-  if(etaUtente < 18){
-    const scontoTotaleMinorenni = prezzoBiglietto * scontoMinorenni;
-    const prezzoBigliettoScontatoMinorenni = prezzoBiglietto - scontoTotaleMinorenni;
-    output = prezzoBigliettoScontatoMinorenni.toFixed(2);
-      
-    document.getElementById('prezzo').innerHTML =`
-      Il prezzo del biglietto è soggetto allo sconto minorenne del ${scontoMinorenni * 100}%, il costo totale è di: <br>
-      <span class="bg-info fw-bold">${output} €</span>
-    `;
-  }
-
-  if (etaUtente >= 65){
-    const scontoTotaleOver = prezzoBiglietto * scontoOver;
-    const prezzoBigliettoScontatoOver = prezzoBiglietto - scontoTotaleOver;
+  if(userNumberKm < 1 || isNaN(userNumberKm)){
+    alert('Si prega di inserire un valore valido nel campo "Km da percorrere"!');
     
-    output = prezzoBigliettoScontatoOver.toFixed(2);
-     
-    document.getElementById('prezzo').innerHTML =`
-      Il prezzo del biglietto è soggetto allo sconto over 65 del ${scontoOver * 100}%, il costo totale è di: <br>
-      <span class="bg-info fw-bold">${output} €</span>
-    `; 
+    ValidData = false;
   }
 
-  if(etaUtente < 1 || etaUtente > 122 || isNaN(etaUtente) ){
-    alert('Inserisci un età valita compresa tra 1 e 122');
-    calcoloValido = false;
+  if(ValidData){
+  
+    if(userAge === 'maggiorenne'){
+      output = ticketCost.toFixed(2); 
+    
+      document.getElementById('name-output').innerHTML =`
+        <span class="fw-bold text-uppercase">${nameSurname} </span>
+      `; 
+      document.getElementById('offert').innerHTML =`
+        <span class="fw-bold">Biglietto Standard</span>
+      `; 
+      document.getElementById('carriage').innerHTML = Math.floor(Math.random() * 10);
+
+      document.getElementById('code').innerHTML = Math.floor(Math.random() * 1000000);
+
+      document.getElementById('cost').innerHTML =`
+        <span class="fw-bold">${output} €</span>
+      `; 
+    }else if(userAge === 'minorenne'){
+      const ticketMinorDiscount = ticketCost - ticketCost * minorDiscount;      
+      output = ticketMinorDiscount.toFixed(2);
+
+      document.getElementById('name-output').innerHTML =`
+        <span class="fw-bold text-uppercase">${nameSurname} </span>
+      `; 
+      document.getElementById('offert').innerHTML =`
+        <span class="fw-bold">${minorDiscount * 100}%</span>
+      `; 
+      document.getElementById('carriage').innerHTML = Math.floor(Math.random() * 10);
+
+      document.getElementById('code').innerHTML = Math.floor(Math.random() * 1000000);
+      
+      document.getElementById('cost').innerHTML =`
+        <span class="fw-bold">${output} €</span>
+      `; 
+    }else{
+      const ticketSeniorDiscount =ticketCost - ticketCost * seniorDiscount;
+      output = ticketSeniorDiscount.toFixed(2);
+       
+      document.getElementById('name-output').innerHTML =`
+        <span class="fw-bold text-uppercase">${nameSurname} </span>
+      `; 
+      document.getElementById('offert').innerHTML =`
+        <span class="fw-bold">${seniorDiscount * 100}%</span>
+      `; 
+      document.getElementById('carriage').innerHTML = Math.floor(Math.random() * 10);
+
+      document.getElementById('code').innerHTML = Math.floor(Math.random() * 1000000);
+      
+      document.getElementById('cost').innerHTML =`
+        <span class="fw-bold">${output} €</span>
+      `; 
+    }
   }
-}
+});
+
+const btn2 = document.getElementById('flush');
+btn2.addEventListener('click', function(){ 
+  document.getElementById('name').value = '';
+  document.getElementById('km').value = '';
+  document.getElementById('name-output').innerHTML = '';
+  document.getElementById('offert').innerHTML = '';
+  document.getElementById('carriage').innerHTML = '';
+  document.getElementById('code').innerHTML = '';
+  document.getElementById('cost').innerHTML = '';
+});
+
+
+
+
+
+
